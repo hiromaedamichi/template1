8a0d0223f77f7bfc848f797be42f5b64fbf820e7
hiromaedamichi <32251487+hiromaedamichi@users.noreply.github.com>
2017-10-24 22:55:59 +0900

    Update LoginDAO.java

--- template1/src/com/internousdev/template/dao/LoginDAO.java
+++ template1/src/com/internousdev/template/dao/LoginDAO.java
@@ -1,5 +1,41 @@
-package com.internousdev.template.dao;
+package com.internous.template.dao;
+
+import com.internous.template.dto.LoginDTO;
 
 public class LoginDAO {
-
-}
+	
+	private DBConnector dbconnector = new DBConnector();
+	
+	private Connection connection = dbConnector.getConnection();
+	
+	private LoginDTO loginDTO = new LoginDTO();
+	
+	
+	public LoginDTO getLonginUserInfo(String loginUserId,String loginPassword) {
+		String sql = "SELECT * FROM login_user_transactionwhere login_id = ? AND login_pass = ?";
+		
+	 
+		try{
+			PrepareadStatement preparedStatement =
+			connection.preparedStatement(sql);
+			preparedStatement.setString(1,loginUserId);
+			preparedStatement.setString(2,loginPassword);
+			
+			if(resultSet.next()) {
+				loginDTO.setLoginId(resultSet.getString("login_id"));
+				loginDTO.setLoginPassword(resultSet.getString("login_pass"));
+				loginDTO.setUserName(resultSet.getString("user_name"));
+				
+				if(!(resultSet.getString("login_id").equals(null))) {
+					loginDTO.setLoginFlg(true);       
+				}
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return loginDTO;
+	}
+	public LoginDTO getLoginDTO() {
+		return loginDTO;
+	}
+	}
